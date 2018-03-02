460e3e670709ba31156fa0c47a381371a244c0fc
dasolhwang <32833990+dasolhwang@users.noreply.github.com>
2018-03-03 02:21:28 +0900

    Update _10845.py

--- _10845.py
+++ _10845.py
@@ -3,40 +3,24 @@ import sys
 r = lambda: sys.stdin.readline().strip()
 n = int(r())
 
-def function(n):
-	queue=[]
+stack =[]
 
-	for i in range(n):
-		command = r()
+for _ in range(n):
+	command = q()
+	if command[:4] == 'push':
+		stack.append(int(command[4:]))
+	
+	elif command == 'pop':
+		if len(stack) ==0: print(-1)
+		else: print(stack[0]); stack = stack[1:]
 
-		if command[:4]=='push':
-			queue.append(int(command[5:]))
-
-		if command=='pop':
-			queue=queue[1:]
-			return queue
-
-		if command=='size':
-			return len(queue)
-
-		if command=='empty':
-			if len(queue)==0:
-				return 1
-			else: return 0
-
-		if command=='front':
-			if len(queue)==0:
-				return -1
-			else:
-				return queue[0]
-
-		if command=='back':
-			if len(queue)==0:
-				return -1
-			else:
-				return queue[len(queue)]
-
-
-for i in range(n):
-	print(function(n))
-		
+	elif command == 'front':
+		if len(stack)>0: print(stack[0])
+		else: print(-1)
+	elif command == 'back':
+		if len(stack)>0: print(stack[-1])
+		else: print(-1)
+	elif command == 'size': print(len(stack))
+	elif command == 'empty':
+		if len(stack) == 0: print(1)
+		else: print(0)

6022c6316783e351ee80491f5ce997665d091972
dasolhwang <dd_sol@khu.ac.kr>
2018-12-05 14:07:56 +0900

    18.12.05

--- 1978.py
+++ 1978.py
@@ -4,7 +4,6 @@
 import sys
 n = int(sys.stdin.readline())
 x = map(int, sys.stdin.readline().split())
-
 cnt = 0
 for i in x:
     k = i
@@ -14,6 +13,21 @@ for i in x:
         cnt += 1
         for i in range(2, k):
             if k % i == 0:
+                cnt -= 1
+                break
+print(cnt)
+
+
+import sys
+_ = int(sys.stdin.readline())
+x = map(int, sys.stdin.readline().split())
+
+cnt = 0
+for i in x:
+    if i > 1:
+        cnt += 1
+        for j in range(2, i):
+            if i % j == 0:
                 cnt -= 1
                 break
 print(cnt)

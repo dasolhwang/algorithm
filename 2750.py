f0316ab00814aac7f723252873dd3b9c8437913a
dasolhwang <dd_sol@khu.ac.kr>
2018-12-05 13:31:40 +0900

    18.12.03

---
+++ 2750.py
@@ -0,0 +1,35 @@
+import sys
+
+n = int(input())
+x = []
+for i in range(n):
+    x.append(int(input()))
+
+# BubbleSort
+for i in range(n):
+    for j in range(n - 1):
+        if x[j] > x[j + 1]:
+            x[j], x[j + 1] = x[j + 1], x[j]
+for i in x:
+    print(i)
+    
+# SelectionSort
+for i in range(n):
+    min_idx = i
+    for j in range(i+1, n):
+        if x[min_idx] > x[j]:
+            min_idx = j
+    if min_idx != i:
+        x[i], x[min_idx] = x[min_idx], x[i]
+    print(x[i])
+
+# InsertionSort
+for i in range(1, n):
+    for j in range(i-1, -1, -1):
+        if x[i] < x[j]:
+            x[i], x[j] = x[j], x[i]
+            i = j
+        else:
+            break
+for i in x:
+    print(i)
